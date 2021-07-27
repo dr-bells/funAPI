546895cb2ee0b71b0a8a28654d89df61f58fd79b
dr bells <48335433+drbellls@users.noreply.github.com>
2021-07-27 09:37:34 +0200

    Upto intro to test

--- Data/DataContext.cs
+++ Data/DataContext.cs
@@ -1,3 +1,4 @@
+using System;
 using funAPI.Models;
 using Microsoft.EntityFrameworkCore;
 
@@ -5,8 +6,36 @@ namespace funAPI.Data
 {
     public class DataContext : DbContext
     {
-        public DataContext(DbContextOptions<DataContext> options) : base(options)
-        { }
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
         public DbSet<Names> Names { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Names>().HasData(
+                new Names { Id = 1, Name = "Tsitsi", DateGenerated = new DateTime(2021, 07, 20, 16, 23, 40), IsBooked = false },
+                new Names
+                {
+                    Id = 2,
+                    Name = "Anorld",
+                    DateGenerated = new DateTime(2021, 07, 20, 09, 24, 40),
+                    DateBooked = new DateTime(2021, 07, 21, 09, 24, 40),
+                    IsBooked = true
+                },
+                new Names
+                {
+                    Id = 3,
+                    Name = "Abjksabdflksdkfbiugbausdigbkjsdbgfui",
+                    DateGenerated = new DateTime(2021, 07, 20, 09, 24, 40),
+                    IsBooked = false
+                },
+                new Names
+                {
+                    Id = 4,
+                    Name = "Panashe",
+                    DateGenerated = new DateTime(2021, 07, 26, 09, 24, 40),
+                    IsBooked = false
+                }
+                );
+        }
     }
 }
---
+++ Migrations/20210727065247_OverrideOnModelBuilder.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace funAPI.Migrations
+{
+    public partial class OverrideOnModelBuilder : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Names",
+                columns: new[] { "Id", "DateBooked", "DateGenerated", "IsBooked", "Name" },
+                values: new object[,]
+                {
+                    { 1, new DateTime(2021, 7, 27, 6, 52, 45, 717, DateTimeKind.Utc).AddTicks(7944), new DateTime(2021, 7, 20, 16, 23, 40, 0, DateTimeKind.Unspecified), false, "Tsitsi" },
+                    { 2, new DateTime(2021, 7, 21, 9, 24, 40, 0, DateTimeKind.Unspecified), new DateTime(2021, 7, 20, 9, 24, 40, 0, DateTimeKind.Unspecified), true, "Anorld" },
+                    { 3, new DateTime(2021, 7, 27, 6, 52, 45, 718, DateTimeKind.Utc).AddTicks(2883), new DateTime(2021, 7, 20, 9, 24, 40, 0, DateTimeKind.Unspecified), false, "Abjksabdflksdkfbiugbausdigbkjsdbgfui" },
+                    { 4, new DateTime(2021, 7, 27, 6, 52, 45, 718, DateTimeKind.Utc).AddTicks(2890), new DateTime(2021, 7, 26, 9, 24, 40, 0, DateTimeKind.Unspecified), false, "Panashe" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Names",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Names",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Names",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Names",
+                keyColumn: "Id",
+                keyValue: 4);
+        }
+    }
+}
